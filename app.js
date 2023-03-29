ac711a2cd24e02e56044c4f98b7ce9ebb252e6ee
rohan.maharjan <rohan.maharjan@gurzu.com>
2023-03-29 13:35:55 +0545

    schemas and resolvers

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
 
 dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery{
+      events: [String!]!
+    }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+    type RootMutation{
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+    rootValue: {
+      events: () => ['Gaming', 'Movies', 'Coding'],
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  }),
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`Started at ${process.env.PORT}`);
